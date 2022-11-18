6dff99b40a29ea6e1c4abb1073525e2a9cc447c0
godh <gustavo.rezende@gft.com>
2022-11-18 20:29:14 -0300

    Object not found exception

--- src/main/java/br/com/godh/apitests/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/godh/apitests/services/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package br.com.godh.apitests.services.impl;
 import br.com.godh.apitests.entities.User;
 import br.com.godh.apitests.repositories.UserRepository;
 import br.com.godh.apitests.services.UserService;
+import br.com.godh.apitests.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public User findById(Integer id) {
         Optional<User> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
     }
 }
