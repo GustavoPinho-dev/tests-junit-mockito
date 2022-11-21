0209c8fc42dfba4974aa8b559647af3eb230ed78
godh <gustavo.rezende@gft.com>
2022-11-21 13:13:16 -0300

    Tests Main class

--- src/main/java/br/com/godh/apitests/ApiTestsApplication.java
+++ src/main/java/br/com/godh/apitests/ApiTestsApplication.java
@@ -1,6 +1,5 @@
 package br.com.godh.apitests;
 
-import br.com.godh.apitests.entities.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,9 +8,6 @@ public class ApiTestsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiTestsApplication.class, args);
-
-		User user = new User(1, "Gustavo", "email@email.com", "1234567890");
-
 	}
 
 }
