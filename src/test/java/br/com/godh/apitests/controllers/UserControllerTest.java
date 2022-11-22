ad3c540cd7e689f448d5559b8c754acde247e097
godh <gustavo.rezende@gft.com>
2022-11-22 17:13:28 -0300

    Tests delete UserController

--- src/test/java/br/com/godh/apitests/controllers/UserControllerTest.java
+++ src/test/java/br/com/godh/apitests/controllers/UserControllerTest.java
@@ -20,7 +20,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class UserControllerTest {
@@ -119,7 +119,17 @@ class UserControllerTest {
     }
 
     @Test
-    void delete() {
+    void whenDeleteThenReturnSuccess() {
+        doNothing().when(service).delete(anyInt());
+
+        ResponseEntity<UserDTO> response = controller.delete(ID);
+
+        assertNotNull(response);
+        assertEquals(ResponseEntity.class, response.getClass());
+
+        verify(service, times(1)).delete(anyInt());
+
+        assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
     }
 
     private void startUser() {
